deae9c1848d7afc21010e52f31f0764ec18564a7
charansinghicreon <charan.singh.icreon@gmail.com>
2018-04-25 18:57:22 +0530

    redirect to login done

--- frontend/src/app/services/afterlogin.service.ts
+++ frontend/src/app/services/afterlogin.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { TokenService } from './token.service';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
 
 @Injectable()
 export class AfterloginService implements CanActivate {
 
-  constructor(private token : TokenService) { }
+  constructor(private token : TokenService, private router : Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean>|Promise<boolean>|boolean {
 
-      return this.token.loggedIn();
+  	  if(this.token.loggedIn()) {
+      	
+      	return this.token.loggedIn();
+  	  }
+
+  	   this.router.navigateByUrl('/login');
+
     }
   
 
